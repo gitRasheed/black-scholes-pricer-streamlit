df3723895cd54e84df8f3194dd0b343ec626afbd
Rasheed <99349713+gitRasheed@users.noreply.github.com>
2024-08-17 15:54:03 +0100

    black scholes class tweaks

--- black_scholes.py
+++ black_scholes.py
@@ -2,31 +2,35 @@ import numpy as np
 from scipy.stats import norm
 
 class BlackScholes:
-    def __init__(self, S, K, T, r, sigma):
-        self.S, self.K, self.T, self.r, self.sigma = S, K, T, r, sigma
+    def __init__(self, S, K, T, r, sigma, q=0):
+        self.S, self.K, self.T, self.r, self.sigma, self.q = S, K, T, r, sigma, q
 
     def calculate_option_price(self, option_type='call'):
-        d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
+        d1 = (np.log(self.S / self.K) + (self.r - self.q + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
         d2 = d1 - self.sigma * np.sqrt(self.T)
 
         if option_type == 'call':
-            price = self.S * norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(d2)
+            price = self.S * np.exp(-self.q * self.T) * norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(d2)
         else:
-            price = self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) - self.S * norm.cdf(-d1)
+            price = self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) - self.S * np.exp(-self.q * self.T) * norm.cdf(-d1)
 
         return price
 
     def calculate_greeks(self):
-        d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
+        d1 = (np.log(self.S / self.K) + (self.r - self.q + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
         d2 = d1 - self.sigma * np.sqrt(self.T)
 
         greeks = {
-            'delta_call': norm.cdf(d1),
-            'delta_put': norm.cdf(d1) - 1,
-            'gamma': norm.pdf(d1) / (self.S * self.sigma * np.sqrt(self.T)),
-            'vega': self.S * np.sqrt(self.T) * norm.pdf(d1),
-            'theta_call': -self.S * norm.pdf(d1) * self.sigma / (2 * np.sqrt(self.T)) - self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(d2),
-            'theta_put': -self.S * norm.pdf(d1) * self.sigma / (2 * np.sqrt(self.T)) + self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(-d2),
+            'delta_call': np.exp(-self.q * self.T) * norm.cdf(d1),
+            'delta_put': -np.exp(-self.q * self.T) * norm.cdf(-d1),
+            'gamma': np.exp(-self.q * self.T) * norm.pdf(d1) / (self.S * self.sigma * np.sqrt(self.T)),
+            'vega': self.S * np.exp(-self.q * self.T) * norm.pdf(d1) * np.sqrt(self.T),
+            'theta_call': -self.S * np.exp(-self.q * self.T) * norm.pdf(d1) * self.sigma / (2 * np.sqrt(self.T)) 
+                          - self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(d2) 
+                          + self.q * self.S * np.exp(-self.q * self.T) * norm.cdf(d1),
+            'theta_put': -self.S * np.exp(-self.q * self.T) * norm.pdf(d1) * self.sigma / (2 * np.sqrt(self.T)) 
+                         + self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) 
+                         - self.q * self.S * np.exp(-self.q * self.T) * norm.cdf(-d1),
             'rho_call': self.K * self.T * np.exp(-self.r * self.T) * norm.cdf(d2),
             'rho_put': -self.K * self.T * np.exp(-self.r * self.T) * norm.cdf(-d2)
         }
